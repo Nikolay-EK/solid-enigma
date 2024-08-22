81ea4302e0b8a75ea2745bd4f17e1efc1744b832
Nikolay-EK <ch3rnow.niko@yandex.ru>
2024-08-22 21:41:59 +0300

    Add files via upload

---
+++ 1 st program.py
@@ -0,0 +1,13 @@
+print(9**0.5 * 5)                           # 1 st program
+
+print(9.99>9.98 and 1000!=1001)             # 2 st program
+
+print(2*2+2) # 3 st program
+print(2*(2+2))# 3 st program
+print(2*2+2 == 2*(2+2)) # 3 st program
+
+name = "123.456" # 4 st program
+print(name [4])
+print(int(123.456))
+print(123.456 * 10)
+

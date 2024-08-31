f9771ecb0b65913b991c21f0415efdd3af9cf718
Nikolay-EK <ch3rnow.niko@yandex.ru>
2024-08-31 11:12:33 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1 , 2 , 3 , 'False'
+print(immutable_var)
+#immutable_var_[1 ]= 200
+#print(immutable_var_[1])
+mutable_list=([25,26,27] ,4)
+print(mutable_list)
+mutable_list[0][0]= 3
+print(mutable_list)

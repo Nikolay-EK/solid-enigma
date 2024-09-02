1b34a37a0d90bc05811ee78a295bd2070e031f1a
Nikolay-EK <ch3rnow.niko@yandex.ru>
2024-09-02 18:54:43 +0300

    Add files via upload

---
+++ module_2_2py.py
@@ -0,0 +1,12 @@
+first = int(input('Введите число: '))
+print(first )
+second = int(input('введите число: '))
+print(second)
+thing = int(input('введите число: '))
+print(thing)
+if first == second and second==thing and first==thing:
+    print(3)
+elif first == second or second == thing or first == thing:
+      print(2)
+elif first!=second or second!= thing or first!=thing:
+    print(0)

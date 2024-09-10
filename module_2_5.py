8827fff96aa169d9903f0a9d582880cd76ee8bbd
Nikolay-EK <ch3rnow.niko@yandex.ru>
2024-09-10 22:46:41 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,23 @@
+def get_matrix (n,m, value):
+    matrix = []
+    for i in range(n):
+        lines=[]
+        matrix.append(lines)
+    for k in range(m):
+        lines.append(value)
+    return matrix
+
+    print(get_matrix)
+
+result1 = (2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
+
+
+
+
+
+

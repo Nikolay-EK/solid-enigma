946cf246b98c02b5ed4a945e01da74f5f6b74128
Nikolay-EK <ch3rnow.niko@yandex.ru>
2024-09-27 08:15:15 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,20 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(15, False)
+print_params(15, False, False)
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+
+values_list = [73.36, 'Строка_2', True ]
+values_dict = {'a': 24, 'b': 'строка_3', 'c': False}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+
+values_list_2 = [55.34, 'Строка_4']
+print_params(*values_list_2, 46)
